717a06b930e25a7ec899a750e586ddcea2235fea
rabin186 <rabinacharya186@gmail.com>
2025-03-04 13:20:57 +0545

    Added file postfix-exp.c

---
+++ dsa/3_postfix-exp.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+
+void infixTOpostfix(char infix[], char post_stack[]);
+int precedency(char c);
+
+int precedency(char c) {
+    if (c == '$')
+        return 4;
+    else if (c == '/' || c == '*')
+        return 3;
+    else if (c == '+' || c == '-')
+        return 2;
+    else
+        return 1;
+}
+
+int main() {
+    char infix[100], postfix[100];  
+    printf("Enter infix expression: ");
+    scanf("%s", infix);  
+
+    infixTOpostfix(infix, postfix);  
+
+    printf("Postfix Expression: %s\n", postfix);
+    printf("\n-----------------------------------\n");
+    printf("Programmed By Rabin Acharya.\n");
+    return 0;
+}
+
+void infixTOpostfix(char infix[], char post_stack[]) {
+    int ps_tos = 0, op_tos = -1;
+    char op_stack[100];
+
+    for (int i = 0; i < strlen(infix); i++) { 
+        char c = infix[i];
+
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            post_stack[ps_tos++] = c;
+        } 
+        else if (c == '(') {
+            op_stack[++op_tos] = c;
+        } 
+        else if (c == ')') {
+            while (op_tos >= 0 && op_stack[op_tos] != '(') {
+                post_stack[ps_tos++] = op_stack[op_tos--];
+            }
+            if (op_tos >= 0) {
+                op_tos--;  // Remove '(' from stack
+            }
+        } 
+        else {  
+            while (op_tos >= 0 && precedency(op_stack[op_tos]) >= precedency(c)) {
+                post_stack[ps_tos++] = op_stack[op_tos--];
+            }
+            op_stack[++op_tos] = c;
+        }
+    }
+
+    while (op_tos >= 0) {  
+        post_stack[ps_tos++] = op_stack[op_tos--];
+    }
+
+    post_stack[ps_tos] = '\0';  // Null-terminate the postfix string
+}
+
+
